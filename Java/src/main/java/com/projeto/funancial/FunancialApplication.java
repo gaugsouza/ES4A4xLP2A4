ba9925688e20f9ebfc943c173e26077f5dadb941
Calébe <calebe.fazzani.facul@gmail.com>
2019-10-04 00:10:33 -0300

    -> Adiçao Java (Spring Boot) + JavaScript (React) -> Configuraçao API Usuarios (Falta codificaçao para criptografar senha) -> Falta configurar React para acessar API e configurar API para receber dados do front end

---
+++ Java/src/main/java/com/projeto/funancial/FunancialApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.funancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunancialApplication.class, args);
+	}
+
+}
