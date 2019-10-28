3fabde89958fd717749ce55fc7ef3af6f9c0bd76
Guilherme Augusto de Souza <guilherme.augusto.souza@ibm.com>
2019-10-28 13:41:27 -0300

    Connection to firebase configured

--- Java/src/main/java/com/projeto/funancial/FunancialApplication.java
+++ Java/src/main/java/com/projeto/funancial/FunancialApplication.java
@@ -1,13 +1,41 @@
 package com.projeto.funancial;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
 @SpringBootApplication
 public class FunancialApplication {
-
+    private static final Logger logger = LogManager.getLogger();
+    private static final String conUrl = "https://funancialprovisorio.firebaseio.com/"; 
+		
 	public static void main(String[] args) {
-		SpringApplication.run(FunancialApplication.class, args);
+		try {
+			FileInputStream serviceAccount =
+					  new FileInputStream("C:\\Users\\GuilhermeAugustodeSo\\Documents\\Pessoal\\Faculdade\\funancialprovisorio-firebase-adminsdk-nef3z-a528a7e69d.json");
+			FirebaseOptions options;
+			logger.info("Inicializando a conexão com o firebase no endereço {}", conUrl);
+			options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			    .setDatabaseUrl(conUrl)
+			    .build();
+			FirebaseApp.initializeApp(options);
+			logger.info("Conexão bem sucedida.");
+			SpringApplication.run(FunancialApplication.class, args);
+		} catch (IOException e) {
+			logger.error("Conexão mal sucedida.");
+			e.printStackTrace();
+		}	
+
+		
 	}
 
 }
