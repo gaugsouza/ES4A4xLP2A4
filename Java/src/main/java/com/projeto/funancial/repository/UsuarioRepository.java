feebe5ba3892b45edf086a8b60e434bc7d87e15d
Guilherme Augusto de Souza <guilherme.augusto.souza@ibm.com>
2019-10-29 15:55:12 -0300

    Removed Firebase added MongoDB

--- Java/src/main/java/com/projeto/funancial/repository/UsuarioRepository.java
+++ Java/src/main/java/com/projeto/funancial/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.projeto.funancial.repository;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.projeto.funancial.model.Usuario;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
-
+public interface UsuarioRepository extends MongoRepository<Usuario, String>{
+	Usuario findBy_id(ObjectId id);
 }
