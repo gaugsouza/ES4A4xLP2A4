0f7c5eca44dcc108d36920692b053d2f9129d805
calebe <calebe.fazzani.facul@gmail.com>
2019-12-04 01:08:20 -0300

    Alterações no back para arrumar bugs. Verificar depois o CORS pois no cadastro funciona e no login não lol

--- Java/src/main/java/com/projeto/funancial/controller/LoginController.java
+++ Java/src/main/java/com/projeto/funancial/controller/LoginController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,13 +24,13 @@ import com.projeto.funancial.transformation.UsuarioTransformation;
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins =  "http://localhost:3000" )
 public class LoginController {
 	private UsuarioService usuarioService;
 	private EncriptadorService encriptadorService;
 	private AuthenticationService authenticationService;
 	private UsuarioTransformation usuarioTransformation;
-	
+
 	public LoginController(UsuarioService usuarioService, EncriptadorService encriptadorService,
 			AuthenticationService authenticationService, UsuarioTransformation usuarioTransformation) {
 		this.usuarioService = usuarioService;
@@ -37,65 +38,69 @@ public class LoginController {
 		this.authenticationService = authenticationService;
 		this.usuarioTransformation = usuarioTransformation;
 	}
-	
-    private final Logger logger = LogManager.getLogger(LoginController.class);
-	
+
+	private final Logger logger = LogManager.getLogger(LoginController.class);
+
 	@PostMapping(value = "/")
 	public ResponseEntity<UsuarioCanonical> efetuaLogin(@RequestBody UsuarioCanonical usuarioCanonical) {
-		if(Optional.ofNullable(usuarioCanonical.getJwt()).isPresent() && 
-				authenticationService.validaToken(usuarioCanonical))
-			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.OK);		
-		
-		Optional<Usuario> usuario = usuarioService.findAll().stream()
-				.filter(u -> usuarioCanonical.getEmail().equals(u.getEmail())).findFirst();			
-		
-		if(!usuario.isPresent())
-			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.NO_CONTENT);	
-		
-		try {
-			if(!encriptadorService.validaSenha(usuarioCanonical.getSenha(), usuario.get().getSenha()))
-				return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.UNAUTHORIZED);
-		
-			usuarioCanonical.setJwt(authenticationService.geraToken(usuarioCanonical));
-			
+		if (Optional.ofNullable(usuarioCanonical.getJwt()).isPresent()
+				&& authenticationService.validaToken(usuarioCanonical))
 			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.OK);
-		} catch(EncriptadorServiceException e) {
-			logger.error("Erro encontrado durante a valida��o da senha informada:\n" + e.getMessage()
-					+ "\nCausa:\n" + e.getCause());
-			
+
+		Optional<Usuario> usuario = usuarioService.findAll().stream()
+				.filter(u -> usuarioCanonical.getEmail().equals(u.getEmail())).findFirst();
+
+		if (!usuario.isPresent())
+			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.NO_CONTENT);
+
+		try {
+			if (!encriptadorService.validaSenha(usuarioCanonical.getSenha(), usuario.get().getSenha()))
+				return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.UNAUTHORIZED);
+
+			usuarioCanonical.setJwt(authenticationService.geraToken(usuarioCanonical));
+
+			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.OK);
+		} catch (EncriptadorServiceException e) {
+			logger.error("Erro encontrado durante a valida��o da senha informada:\n" + e.getMessage() + "\nCausa:\n"
+					+ e.getCause());
+
 			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.INTERNAL_SERVER_ERROR);
-		} catch(AuthenticationServiceException e) {
-			logger.error("Erro encontrado durante a gera��o de token: \n" + e.getMessage()
-			+ "\nCausa:\n" + e.getCause());
-			
+		} catch (AuthenticationServiceException e) {
+			logger.error("Erro encontrado durante a gera��o de token: \n" + e.getMessage() + "\nCausa:\n"
+					+ e.getCause());
+
 			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	@PostMapping(value = "/cadastro")
 	public ResponseEntity<UsuarioCanonical> efetuaCadastro(@RequestBody UsuarioCanonical usuarioCanonical) {
 		Optional<Usuario> usuario = usuarioService.findAll().stream()
-				.filter(u -> usuarioCanonical.getEmail().equals(u.getEmail())).findFirst();			
-		
-		if(usuario.isPresent())
+				.filter(u -> usuarioCanonical.getEmail().equals(u.getEmail())).findFirst();
+
+		if (usuario.isPresent())
 			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.CONFLICT);
-		
+
 		try {
+			usuarioCanonical.set_id(ObjectId.get());
 			usuarioCanonical.setSenha(encriptadorService.geraSenhaEncriptada(usuarioCanonical.getSenha()));
 			usuarioCanonical.setJwt(authenticationService.geraToken(usuarioCanonical));
+
+			Usuario usuarioSalvo = usuarioService.save(usuarioTransformation.convert(usuarioCanonical));
+			UsuarioCanonical canonicalSalvo = usuarioTransformation.convert(usuarioSalvo);
+			canonicalSalvo.setJwt(usuarioCanonical.getJwt());
 			
-			return new ResponseEntity<UsuarioCanonical>(
-					usuarioTransformation.convert(usuarioService.save(usuarioTransformation.convert(usuarioCanonical))),
-					HttpStatus.OK);
-		} catch(EncriptadorServiceException e) {
-			logger.error("Erro encontrado durante a gera��o da senha informada:\n" + e.getMessage()
-			+ "\nCausa:\n" + e.getCause());
-	
-			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.INTERNAL_SERVER_ERROR); 
-		} catch (AuthenticationServiceException  e) {
-			logger.error("Erro encontrado durante a gera��o de token: \n" + e.getMessage()
-			+ "\nCausa:\n" + e.getCause());
+			return new ResponseEntity<UsuarioCanonical>(canonicalSalvo, HttpStatus.OK);
 			
+		} catch (EncriptadorServiceException e) {
+			logger.error("Erro encontrado durante a gera��o da senha informada:\n" + e.getMessage() + "\nCausa:\n"
+					+ e.getCause());
+
+			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (AuthenticationServiceException e) {
+			logger.error("Erro encontrado durante a gera��o de token: \n" + e.getMessage() + "\nCausa:\n"
+					+ e.getCause());
+
 			return new ResponseEntity<UsuarioCanonical>(usuarioCanonical, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
